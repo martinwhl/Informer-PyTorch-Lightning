4753b15e16bdd5b89448b3858abf3b8aedb0e3bc
martinwhl <asteroid02@126.com>
2021-04-12 15:52:12 +0800

    fix(datasets): fix wrong weekday value returned by time_features() function

--- utils/time_features.py
+++ utils/time_features.py
@@ -93,7 +93,7 @@ def time_features(dates, time_encoding=True, frequency: str='h'):
     if time_encoding == False:
         dates['month'] = dates.date.apply(lambda row: row.month, 1)
         dates['day'] = dates.date.apply(lambda row: row.day, 1)
-        dates['weekday'] = dates.date.apply(lambda row: row.weekday, 1)
+        dates['weekday'] = dates.date.apply(lambda row: row.weekday(), 1)
         dates['hour'] = dates.date.apply(lambda row: row.hour, 1)
         if frequency == 't':
             dates['minute'] = dates.date.apply(lambda row: row.minute, 1)
